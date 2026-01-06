5bee8dc3d1507003a946fcf72723da28b81fa163
Updater Robot <robot@nowhere.invalid>
2026-01-06 18:34:05 +0000

    SYSTEM-CONFIG : Update GMSCORE to 250932022
    Update FDroid to 1.23.1
    
    Update GMSCORE to 250932022
    
    Update core applications
    
    Remove : FDroid
    
    Remove : additional apps
    
    clean up and arrange the package properly
    -remove IchnaeaNlpBackend
    -NominatimGeocoderBackend
    -additional_repos.xml

--- microg.mk
+++ microg.mk
@@ -1,9 +1,8 @@
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/microg
+# Core System Applications (4 apps)
 PRODUCT_PACKAGES += \
 	GmsCore \
 	GsfProxy \
-	FakeStore \
-	IchnaeaNlpBackend \
-	NominatimGeocoderBackend \
-	FDroid \
-	FDroidPrivilegedExtension \
-	additional_repos.xml
+	FakeStore
